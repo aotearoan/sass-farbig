8b76eea94c982829aac858416c8d48b8694d6991
aotearoan <andrew@aotearoan.com>
2023-03-24 10:27:32 +0000

    gh-pages

---
+++ precache-manifest.de942b3a3d12b5b905c2a0fc5b30f672.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a2337a99ac6e4ab7bff",
+    "url": "/sass-farbig/css/app.1ba8d4d7.css"
+  },
+  {
+    "revision": "49bb89910630e64143fe4dd349af302f",
+    "url": "/sass-farbig/index.html"
+  },
+  {
+    "revision": "3a2337a99ac6e4ab7bff",
+    "url": "/sass-farbig/js/app.7182fe96.js"
+  },
+  {
+    "revision": "5a6ebe63dcedde354dbe",
+    "url": "/sass-farbig/js/chunk-vendors.35326c5f.js"
+  },
+  {
+    "revision": "91a5b1449e973a0036c6c51055292050",
+    "url": "/sass-farbig/manifest.json"
+  }
+]);
